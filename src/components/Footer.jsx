f78df246ff041c7b0fe4529f5a036ca7165d24d2
Martin <martin.c.nelson8@gmail.com>
2021-01-09 22:51:57 -0600

    fix server.js file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,11 @@ import styled from 'styled-components';
 const StyledFooter = styled.div`
     padding: 2rem;
     text-align: center;
-
+    @media (max-width: 550px) {
+        p{
+            font-size: 10px;
+        }
+    }
 `
 
 export default function Footer() {
